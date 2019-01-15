03ea5dd77e4a75e966999498ad519c267f20f8bc
Felipe Trevisan <fel.trevisan@gmail.com>
2019-01-15 11:50:40 -0300

    Inclusão de regras de negócio

--- index.js
+++ index.js
@@ -15,22 +15,55 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerYaml));
 
 //Definição de rotas
 app.post('/users', function (request, response, next) {
+    if (!request.body.name || !request.body.phone || !request.body.email || !request.body.password) {
+        return response.status(400).send({ 'message': 'Dados obrigatórios não informados.' });
+    }
+
+    if (request.body.password.length < 5) {
+        return response.status(400).send({ 'message': 'A senha deve conter no mínimo 5 caracteres.' });
+    }
+
     return response.status(201).send({ 'message': 'Usuário criado.' });
 });
 
 app.get('/users', function (request, response, next) {
+    if (!request.query.limit || !request.query.page) {
+        return response.status(400).send({ 'message': 'Dados obrigatórios não informados.' });
+    }
+
     return response.status(200).send({ 'message': 'Usuários listados.' });
 });
 
-app.get('/users/:id', function (request, response, next) {
+app.get('/users/:_id', function (request, response, next) {
+    if (!request.params._id && request.params.id !== 'undefined') {
+        return response.status(400).send({ 'message': 'Dados obrigatórios não informados.' });
+    }
+
     return response.status(200).send({ 'message': 'Usuário detalhado.' });
 });
 
-app.delete('/users/:id', function (request, response, next) {
+app.delete('/users/:_id', function (request, response, next) {
+    if (!request.params._id && request.params.id !== 'undefined') {
+        return response.status(400).send({ 'message': 'Dados obrigatórios não informados.' });
+    }
+
     return response.status(200).send({ 'message': 'Usuário removido.' });
 });
 
-app.patch('/users/:id', function (request, response, next) {
+app.patch('/users/:_id', function (request, response, next) {
+    if (
+        (!request.params._id && request.params.id !== 'undefined') ||
+        (
+            !request.body.name && !request.body.phone && !request.body.email && !request.body.password
+        )
+    ) {
+        return response.status(400).send({ 'message': 'Dados obrigatórios não informados.' });
+    }
+
+    if (request.body.password && request.body.password.length < 5) {
+        return response.status(400).send({ 'message': 'A senha deve conter no mínimo 5 caracteres.' });
+    }
+
     return response.status(200).send({ 'message': 'Usuário atualizado.' });
 });
 
